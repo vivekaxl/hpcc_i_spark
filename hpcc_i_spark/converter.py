f09c551fc94e7de0ceab80df40505a9de7489bf1
Vivek Nair <vivekaxl@gmail.com>
2016-11-30 18:38:13 -0500

    final push

---
+++ hpcc_i_spark/converter.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+
+def find_record_structure(logical_filename, thor_ip):
+    import urllib2
+    import json
+    ''' get record structure '''
+    records = []
+    
+    # Building the url which can be used extract the file layout of the logical filename
+    url = "http://" + thor_ip + ":8002/WsEcl/submit/query/thor/getfilelayout/json?"
+    url += "esp_server=thor_ip&username=&password=&logical_filename="
+    url += logical_filename
+    
+    # Download the content of the page
+    response = urllib2.urlopen(url)
+    html = response.read()
+    
+    # Extract the record set from the json downloaded from the url
+    j = json.loads(html)
+    xml_string = j['getfilelayoutResponse']['Results']['FileLayout']['Row'][-1]
+    xml_string = str(xml_string['FileLayout'])
+    
+    # get breaks
+    breaks = xml_string.split("<br>")
+    
+    # removing exports and end
+    breaks = breaks[1:-1]
+    for break_tag in breaks:
+        records.append(break_tag.strip())
+    return '{' + "".join(records) + '}'
+
+
+def get_content(logical_filename, thor_ip, no_sample):
+    import urllib2
+    import json
+    import xmltodict
+
+    # Find the record structure of the logical filename
+    record_string = find_record_structure(logical_filename, thor_ip)
+    url = "http://" + thor_ip + ":8002/WsEcl/submit/query/thor/runprogram/json?"
+    url += "recordds=\'" + record_string + "\'&"
+    url += "filename=\'" + logical_filename + "\'&"
+    url += "sample_no=" + str(no_sample)
+    
+    # Replacing the whitespace in the url with %20
+    url = url.replace(' ', '%20')
+    
+    # Download the content of the page
+    response = urllib2.urlopen(url)
+    html = response.read()
+    
+    # Parsing the response
+    j = json.loads(html)
+    xml_string = '<root>' + str(j["runprogramResponse"]["Results"]["op_GetWorkunitResult"]["Row"][-1]["Result"]) + '</root>'
+
+    o = xmltodict.parse(xml_string)
+    rows = o["root"]["Dataset"]["Row"]
+    content = [[row[key] for key in row.keys()] for row in rows]
+    return content
